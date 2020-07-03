1d19e027496c08b23b0dd16e809ea485beb3cac4
Elliot Cater <hello@caters.net>
2020-07-03 15:36:04 +0100

    Initial Commit

---
+++ QsirchMac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  QsirchMac
+//
+//  Created by Elliot Cater on 03/07/2020.
+//  Copyright © 2020 Caters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
