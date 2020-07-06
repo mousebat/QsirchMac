7556943cab15e78ca182b96b17725fb5166d71e7
Elliot Cater <hello@caters.net>
2020-07-06 20:40:28 +0100

    Initial Commit

--- QsirchMac/AppDelegate.swift
+++ QsirchMac/AppDelegate.swift
@@ -9,31 +9,43 @@
 import Cocoa
 import SwiftUI
 
+// MARK: - Override search Window
+class SWindow: NSWindow {
+    override var canBecomeKey:Bool {
+        return true
+    }
+}
+
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
-
+    var window: SWindow!
+    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let searchView = SearchView()
 
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+        // Create the window and set the content view.
+        window = SWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 850, height: 500),
+            styleMask: [.resizable],
             backing: .buffered, defer: false)
         window.center()
         window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
+        window.contentView = NSHostingView(rootView: searchView)
         window.makeKeyAndOrderFront(nil)
+        window.isMovableByWindowBackground = true
+        window.titlebarAppearsTransparent = true
     }
+    
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    
+    
 
 }
-
---
+++ QsirchMac/SearchView.swift
@@ -0,0 +1,115 @@
+//
+//  ContentView.swift
+//  QsirchMac
+//
+//  Created by Elliot Cater on 03/07/2020.
+//  Copyright © 2020 Caters. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+// This extension removes the focus ring entirely.
+extension NSTextField {
+    open override var focusRingType: NSFocusRingType {
+        get { .none }
+        set { }
+    }
+}
+
+struct SomeButtons: View {
+    @State private var thisToggle:Bool = false
+    @State private var thatToggle:Bool = false
+    @State private var theotherToggle:Bool = false
+    var body: some View {
+        VStack{
+            Divider()
+            HStack(alignment: .center, spacing: 20) {
+                Toggle(isOn: $thisToggle){
+                   Text("This Drive")
+                      
+                }.toggleStyle(SwitchToggleStyle())
+                Divider().frame(height: 20)
+                Toggle(isOn: $thatToggle){
+                   Text("That Drive")
+                      
+                }.toggleStyle(SwitchToggleStyle())
+                Divider().frame(height: 20)
+                Toggle(isOn: $theotherToggle){
+                   Text("The Other Drive")
+                      
+                }.toggleStyle(SwitchToggleStyle())
+                Divider().frame(height: 20)
+                Spacer()
+            }.padding(.horizontal)
+        }
+        
+    }
+}
+
+struct SearchView: View {
+    @State var searchField = ""
+    var body: some View {
+        Group {
+            VStack {
+                HStack(alignment: .center, spacing: 10) {
+                    Text("􀊫").font(.largeTitle).foregroundColor(.primary)
+                    TextField("Search", text: $searchField)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .foregroundColor(.primary)
+                        .background(Color.clear)
+                        .font(Font.system(size: 25, weight: .light, design: .default))
+                    .fixedSize()
+                    Spacer()
+                        
+                }
+                .foregroundColor(.secondary)
+                .padding([.top, .leading, .trailing], 20.0)
+                .background(Color.clear)
+            }
+            SomeButtons()
+                    
+            HSplitView() {
+                NavigationView {
+                    List() {
+                        Spacer()
+                        ForEach (0..<51) { i in
+                            // Navigation Link?
+                            HStack(alignment: .center) {
+                                //Insert Icon for File Here
+                                Text("􀈿").font(Font.system(size:22)).foregroundColor(.primary)
+                                VStack(alignment: .leading) {
+                                    Text("File Name \(i)").font(Font.system(size: 16, weight: .semibold, design: .default))
+                                    Text("File Path \(i)").font(Font.system(size: 12, weight: .light, design: .default))
+                                }
+                                
+                            }.background(Color.white)
+                            
+                            Divider()
+                        }
+                    }.frame(minWidth: 500, minHeight: 400)
+                }
+                
+                List() {
+                    //Display Selected File Details Here
+                    Text("Content")
+                    Text("Content")
+                    Text("Content")
+                    Text("Content")
+                }.frame(minWidth: 300, idealWidth: 300)
+            }.background(Color.white)
+                
+        }
+    }
+    
+}
+
+
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
+
